ab3faae703da796719e7022a7077b7d5a85be728
wendy <460130566@qq.com>
2019-05-29 14:14:04 +0800

    第二次修改

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ App({
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
     })
-    // 获取用户信息
     wx.getSetting({
       success: res => {
         if (res.authSetting['scope.userInfo']) {
@@ -21,7 +20,6 @@ App({
               this.globalData.userInfo = res.userInfo
 
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
               if (this.userInfoReadyCallback) {
                 this.userInfoReadyCallback(res)
               }
